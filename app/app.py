4037852403e3ed2bcdfa4644e4bfff15ab878cb2
福士 義和 <fukushi_yoshikazu@ca-adv.co.jp>
2021-03-25 15:13:07 +0900

    DBから取得したレコードを表示する

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from models.models import TaskContent
+from models.database import db_session
+from sqlalchemy import exc, desc
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    task_list = TaskContent.query.filter_by(status=0).order_by(desc(TaskContent.create_datetime)).all()
+    return render_template("list.html",tasks=task_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
